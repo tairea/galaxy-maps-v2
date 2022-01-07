08ec6949133311c57584d306c09e6fec13408cc5
Ian Tairea <waipuna@gmail.com>
2022-01-07 17:43:19 +1300

    WIP testing many line charts with single legend

---
+++ src/components/Chart.vue
@@ -0,0 +1,36 @@
+<!-- Chart.vue -->
+<template>
+  <div>
+    <canvas v-once ref="canvas" style="height: 100%; width: 100%"></canvas>
+  </div>
+</template>
+
+<script>
+import { Chart, registerables } from "chart.js";
+Chart.register(...registerables);
+
+// Chart.defaults.elements.point.radius = 0;
+
+export default {
+  props: {
+    chartType: String,
+    chartData: Object,
+    chartOptions: Object,
+  },
+  methods: {
+    chartConstructor(chartType, chartData, chartOptions) {
+      const chartElement = this.$refs.canvas;
+      const chart = new Chart(chartElement, {
+        type: chartType,
+        data: chartData,
+        options: chartOptions,
+      });
+    },
+  },
+
+  mounted() {
+    let { chartType, chartData, chartOptions } = this;
+    this.chartConstructor(chartType, chartData, chartOptions);
+  },
+};
+</script>
--- src/views/AllStudentsView.vue
+++ src/views/AllStudentsView.vue
@@ -38,7 +38,9 @@
     <div id="main-section">
       <div id="progression-panel">
         <h2 class="progression-label">Student progression</h2>
-        <StudentProgressionChartJs3 />
+        <div class="progression-charts">
+          <StudentProgressionChartJs3 />
+        </div>
       </div>
     </div>
 
@@ -263,7 +265,7 @@ export default {
     height: 80%;
     border: 1px solid var(--v-missionAccent-base);
     margin-top: 30px;
-    padding: 20px;
+
     // background: var(--v-baseAccent-base);
     position: relative;
     backdrop-filter: blur(2px);
@@ -281,6 +283,10 @@ export default {
       color: var(--v-background-base);
       padding: 0px 20px 0px 5px;
       clip-path: polygon(0 0, 100% 0, 85% 100%, 0% 100%);
+    }
+
+    .progression-charts {
+      // padding: 20px;
     }
   }
 }
