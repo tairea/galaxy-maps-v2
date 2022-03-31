ed44cd99e70ce18cc683058bb6551cf51861459d
Ian Tairea <waipuna@gmail.com>
2022-04-01 10:37:58 +1300

    landing page video background

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,16 +1,22 @@
 <template>
   <div class="bg">
-    <p class="gm-title">GALAXY MAPS</p>
-    <p class="overline">Galaxy Maps is a new digital learning experience</p>
-    <p class="overline">Galaxy Maps are branching paths of learning</p>
-    <p class="overline">Using Galaxy Maps you can navigate your learning</p>
+    <div class="landing-content">
+      <p class="gm-title">GALAXY MAPS</p>
+      <p class="overline">Galaxy Maps is a new digital learning experience</p>
+      <p class="overline">Galaxy Maps are branching paths of learning</p>
+      <p class="overline">Using Galaxy Maps you can navigate your learning</p>
 
-    <v-btn outlined color="baseAccent" class="mt-8" :to="{ path: 'login' }">
-      <!-- <v-icon small>
+      <v-btn outlined color="baseAccent" class="mt-8" :to="{ path: 'login' }">
+        <!-- <v-icon small>
         mdi-account-multiple-plus
       </v-icon> -->
-      SIGN IN TO GALAXY MAPS
-    </v-btn>
+        SIGN IN TO GALAXY MAPS
+      </v-btn>
+      <!-- Background maps video -->
+    </div>
+    <video id="background-video" autoplay loop muted>
+      <source src="../assets/gm-maps.mp4" type="video/mp4" />
+    </video>
   </div>
 </template>
 
@@ -42,7 +48,28 @@ export default {
   align-items: center;
   justify-content: center;
   flex-direction: column;
+
   height: 100vh;
   width: 100vw;
+
+  .landing-content {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    z-index: 1;
+  }
+}
+
+#background-video {
+  width: 100vw;
+  height: 100vh;
+  object-fit: cover;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  z-index: 0;
 }
 </style>
