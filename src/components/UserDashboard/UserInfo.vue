ef4ccc6fb3ebecdce35bd01f5d6d15f65e252fa3
Ian Tairea <waipuna@gmail.com>
2024-01-23 18:25:43 -1000

    adding xp points to db (#101)
    * adding xp points to db
    
    * ui to add and remove xp points
    
    * xp points added to cohort view

--- src/components/UserDashboard/UserInfo.vue
+++ src/components/UserDashboard/UserInfo.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="d-flex flex-column">
+    <!-- Student photo -->
     <StudentAvatar :size="200" />
+    <!-- Student xp points -->
+    <XpPointsDialog
+      :person="person"
+      :xpPoints="person.xpPointsTotal ? person.xpPointsTotal : 0"
+      :canManageXpPoints="isAdmin"
+    />
     <div class="student-info">
       <!-- Student name -->
       <div>
@@ -47,6 +54,7 @@
 
 <script>
 import StudentAvatar from "@/components/UserDashboard/UserInfo/StudentAvatar.vue";
+import XpPointsDialog from "@/components/Dialogs/XpPointsDialog.vue";
 import StudentEditDialog from "@/components/Dialogs/StudentEditDialog.vue";
 import Cohort from "@/components/Reused/Cohort.vue";
 import { db, storage } from "@/store/firestoreConfig";
@@ -61,6 +69,7 @@ export default {
     StudentAvatar,
     StudentEditDialog,
     Cohort,
+    XpPointsDialog,
   },
   async mounted() {
     if (!this.user.data.admin) {
@@ -69,6 +78,9 @@ export default {
   },
   computed: {
     ...mapState(useRootStore, ["person", "cohorts", "user"]),
+    isAdmin() {
+      return this.user.data.admin;
+    },
   },
   data() {
     return {};
